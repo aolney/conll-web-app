17db7a925952112ced63c041b58f7a931b2c0768
Noah C <nbcoomer@gmail.com>
2018-01-31 11:57:44 -0600

    mkToken function

--- src/main/java/edu/memphis/iis/conllwebapp/CoNLLReader.java
+++ src/main/java/edu/memphis/iis/conllwebapp/CoNLLReader.java
@@ -6,6 +6,7 @@
 package edu.memphis.iis.conllwebapp;
 
 import java.io.*;
+import java.util.Arrays;
 import java.util.Map;
 import org.clulab.processors.*;
 import org.clulab.struct.*;
@@ -31,6 +32,7 @@ public class CoNLLReader {
     
     class CoNLLToken{
         String word;
+        // pos might be an int
         String pos;
         String lemma;
         int pred;
@@ -43,17 +45,39 @@ public class CoNLLReader {
         */
     }
     
-    public Document readCoNLL(File file, Document doc){
+    //output is of Document type for now, likely will be changed before final version
+    public Document readCoNLL(String filepath, Document doc) throws FileNotFoundException, IOException{
         
         //open the file
+        File file = new File(filepath);
+        BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream(file)));
         
-        // create an array of all sentences of the document
+         // create an array of all sentences of the document
             // this should be an array of arrays that contain CoNLLToken
         // create an array of of CoNLLToken that will represent each individual sentence
         
+        CoNLLToken sentence[];
+        CoNLLToken sentences[][];
+        
         //set all base values to 0
+        argConflictCount = 0;
+        multiPredCount = 0;
+        argCount = 0;
+        predCount = 0;
+        tokenCount = 0;
+        sentCount = 0;
+        hyphCount = 0;
         
         // read all the sentences and collapse all the hyphens
+        String line;
+        while((line = br.readLine()) != null){
+            line = line.trim();
+            if (line.length() > 0){
+                String[] bits = line.split("\\t");
+                assert(bits.length >= 11); //may need to do some additional testing
+                
+            }
+        }
         
         // close the file and print some debug information
         
@@ -70,5 +94,44 @@ public class CoNLLReader {
         
     }
     
+    // this is the function that may need to be edited a bit to work with our server
+    // 
+    //
+    public CoNLLToken mkToken(String[] bits){
+        
+        CoNLLToken output = new CoNLLToken();
+        output.word = bits[1];
+        output.pos = bits[4]; // might be 3
+        output.lemma = bits[2];
+        
+        // ---> FLAG <----
+        //might run into a trainwreck here
+        // this is a new feature, but i think it will work
+        int head = Integer.parseInt(bits[8]);
+        String depLabel = bits[10];
+        output.dep.put(head, depLabel);
+        
+        // -----> FLAG THIS <------
+        // what if there is no bits[13]
+        // i think this will work, conll2009 reference matches
+        try{
+            String test = bits[13]; //10
+            if(test.equals("_"))
+                output.pred = 0;
+            else
+                output.pred = 1;
+        }
+        catch (IndexOutOfBoundsException e){
+            System.out.println("Error processing predicate, indices out of bounds");
+            output.pred = 0;
+        }
+        // take all members of bits that are [14] and up and put them in a new array
+        // --> FLAG <---
+        // this part may be problematic
+        String[] tmp = Arrays.copyOfRange(bits, 14, bits.length);
+        output.frameBits = tmp;
+        
+        return output;
+    }
     
 }
